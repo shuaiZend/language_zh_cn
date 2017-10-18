89bf005f092188c46e125f7361ca67e6a69c00ab
Wijzijn.Guru <info@wijzijn.guru>
2017-10-18 20:25:45 +0000

    Auto translate 18/10/2017 20:03:30

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
-    'magentotwotranslations_zh_hans_cn',
+    'magentotwotranslations_zh_cn',
     __DIR__
 );
